2f57b62893342f22f6776309bde38106657dfad1
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-02-28 15:12:58 +0100

    FEAT my-app added

---
+++ my-app/src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg">
+
+    {{ appTitle }}
+
+  </div>
+</template>
+  
+<script>
+export default {
+
+  data() {
+
+    return {
+
+      appTitle: 'La Mia Prima App con Vite'
+
+    }
+
+  }
+
+}
+</script>
+
+<style>
+.bg {
+  background-color: lightskyblue;
+  color: black;
+}
+</style>
